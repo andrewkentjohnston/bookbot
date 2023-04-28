a099dafb04fe481dbf95ec9397ab2620b731fe02
drewbear82 <andrewkentjohnston@gmail.com>
2023-04-28 10:52:56 -0600

    fixed for loop to accunt for words that contained the same letter more than once.

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ word_dict = {}
 for letter in alphabet:
     letter_count = 0
     for word in words:
-        if letter in word.lower():
-            letter_count += 1
-        word_dict[letter] = letter_count
+        lower_word = word.lower()
+        for character in lower_word:
+            if character == letter:
+                letter_count += 1
+    word_dict[letter] = letter_count
 
 print(f"--- Begin report of books/frankenstein.txt ---")
 print(f"{len(words)} found in the document\n")
